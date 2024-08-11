02bd99df8606fa3d5a99827503b26ebd1c0801ca
DimitriosStergiou <stergioudemetrius@gmail.com>
2024-08-11 10:08:18 +0300

    Final commit: Added background color, centered images, and finalized styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import HTMLFlipBook from 'react-pageflip';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import Page4 from './Page4';
+import Page5 from './Page5';  // Projects
+import Page6 from './Page6';  // Certifications
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HTMLFlipBook width={600} height={800} showCover={true}>
+        <div className="page"><Page1 /></div>
+        <div className="page"><Page2 /></div>
+        <div className="page"><Page3 /></div>
+        <div className="page"><Page4 /></div>
+        <div className="page"><Page5 /></div>
+        <div className="page"><Page6 /></div>
+      </HTMLFlipBook>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
---
+++ src/Page5.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Page5() {
+  return (
+    <div className="page">
+      <h2>Projects</h2>
+
+      <p>
+        <a href="https://github.com/DimitriosStergiou" target="_blank" rel="noopener noreferrer">
+          https://github.com/DimitriosStergiou
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Page5;
