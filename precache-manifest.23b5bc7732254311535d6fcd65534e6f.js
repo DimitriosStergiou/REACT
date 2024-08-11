f529ab9336b9da355a12dedb381a5807c0640c0d
DimitriosStergiou <stergioudemetrius@gmail.com>
2024-08-11 10:38:50 +0300

    Updates

---
+++ precache-manifest.23b5bc7732254311535d6fcd65534e6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29eec0df3b0bbf514e9e03293adc8062",
+    "url": "/REACT/index.html"
+  },
+  {
+    "revision": "783d154bc91e101a176b",
+    "url": "/REACT/static/css/main.7c4b38f7.chunk.css"
+  },
+  {
+    "revision": "26e17400d773839f5512",
+    "url": "/REACT/static/js/2.06103ced.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/REACT/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "783d154bc91e101a176b",
+    "url": "/REACT/static/js/main.db498f54.chunk.js"
+  },
+  {
+    "revision": "038030afe245769d812b",
+    "url": "/REACT/static/js/runtime~main.c6b40c98.js"
+  },
+  {
+    "revision": "8e1d1916dc951fa090b1b9de6e4967dd",
+    "url": "/REACT/static/media/my-photo.8e1d1916.jpg"
+  }
+]);
